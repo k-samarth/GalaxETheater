36bbac931965a0e1d22bd2cd7f4f1b0f40f4ee37
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-11-22 16:40:20 +0530

    Provider Change

---
+++ src/component/Provider.js
@@ -0,0 +1,88 @@
+import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
+import Header from "../component/Header";
+import Theater from "../component/Theater";
+import Filter from "../component/Filter";
+import Footer from "../component/Footer";
+import logo from "../images/Logo.png";
+import React from "react";
+
+function Provider() {
+    var name = "Samarth";
+    var userType = "ADMIN";
+    // var userType = "USER";
+    var TheaterName = "GalaxE Movie Theater";
+    var TheaterDesc = "4DX, Nexus (Formerly Forum), Koramangala";
+    var TheaterDetails =
+      "Nexus Mall, 21-22, Adugodi Main Road, Koramangala, Chikku Lakshmaiah Layout, Bengaluru, Karnataka 560095, India";
+
+  return (
+    <ChakraProvider>
+      <Header name={name} />
+      <Filter userType={userType} />
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={8}
+        align="center"
+        margin="0% 5%"
+      >
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <Theater
+            userType={userType}
+            TheaterName={TheaterName}
+            TheaterDesc={TheaterDesc}
+            TheaterDetails={TheaterDetails}
+            logo={logo}
+          />
+        </GridItem>
+      </Grid>
+      <Footer />
+    </ChakraProvider>
+  );
+}
+
+export default Provider;
